6c6ae6b784bc37232f3e92f8561fe7a79077ae69
FluryWaana <contact@apicoulet.com>
2020-01-06 23:41:44 +0100

    Correction route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -14,6 +14,7 @@ class UserController extends Controller
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+            'categories'      => $this->getCategories()
         ]);
     }
 }
