c3f9185d739addc23d7a11c804115764b838bf9c
FluryWaana <contact@apicoulet.com>
2020-01-08 10:19:08 +0100

    gestion du panier

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -31,6 +31,7 @@ class PanierController extends Controller
     {
         $repo_article = $this->getDoctrine()->getRepository( Article::class );
 
+        // Décode le JSON en entrée
         $data = json_decode( $request->getContent(), true );
 
         $form = $this->createForm(PanierAddArticleType::class );
@@ -40,6 +41,10 @@ class PanierController extends Controller
             // Récupération de l'article
             $article = $repo_article->find($data['article_reference']);
 
+            // Nombre d'article total dans le panier
+            $quantie_article = 0;
+
+            // Vérifie si l'article n'existe pas
             if( is_null( $article ) )
             {
                 $form->addError( new FormError('L\'article avec la référence ' . $data['article_reference'] . ' n\'existe pas'));
@@ -53,6 +58,10 @@ class PanierController extends Controller
                 // Si l'article à été ajouté au cookie
                 $add = false;
 
+                /**
+                 * Si le panier est vide alors on créer une session contenant les articles
+                 * sinon on vérifie si l'article ajouté existe déjà dans le panier.
+                 */
                 if( is_null( $panier ) )
                 {
                     $session->set('panier', [
@@ -61,16 +70,22 @@ class PanierController extends Controller
                             'quantite' => $data['article_quantite']
                         ]
                     ]);
+
+                    $quantie_article += $data['article_quantite'];
+                    $session->set('panier_count',  $quantie_article);
                 }
                 else
                 {
+                    // Parcours le panier
                     foreach( $panier as $key => $value )
                     {
+                        // Si l'article ajouté existe déjà dans le panier
                         if( $value['article']->getArticleReference() === $article->getArticleReference() )
                         {
                             $panier[$key]['quantite'] += $data['article_quantite'];
                             $add = true;
                         }
+                        $quantie_article += $panier[$key]['quantite'];
                     }
 
                     if( ! $add )
@@ -79,9 +94,11 @@ class PanierController extends Controller
                             'article'  => $article,
                             'quantite' => $data['article_quantite']
                         ];
+                        $quantie_article += $panier[$key]['quantite'];
                     }
 
                     $session->set('panier', $panier);
+                    $session->set('panier_count',  $quantie_article);
                 }
 
                 return $this->json([
@@ -95,5 +112,57 @@ class PanierController extends Controller
             'status' => 'error',
             'errors' => $form->getErrors(true)
         ],400);
+    }
+
+    /**
+     * @Route("/panier/article/{id}", name="panier_del_article", methods={"DELETE"})
+     * @param Request $request
+     * @return Response
+     */
+    public function removeArticle( Request $request, $id ) : Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+
+        $article = $repo->find( $id );
+
+        if( is_null( $article ) )
+        {
+            return $this->json([
+                'status'  => 'Not Found',
+                'message' => 'L\'article ' . $id . ' n\'existe pas'
+            ],404);
+        }
+
+        // Récupération de la session utilisateur
+        $session      = $request->getSession();
+        $panier       = $session->get('panier');
+        $panier_count = $session->get('panier_count');
+
+        foreach( $panier as $key => $value )
+        {
+            // Si l'article ajouté existe déjà dans le panier
+            if( $value['article']->getArticleReference() === $article->getArticleReference() )
+            {
+                $panier_count -= $panier[$key]['quantite'];
+                unset($panier[$key]);
+
+            }
+        }
+
+        if( count( $panier ) == 0 )
+        {
+            $session->remove('panier');
+        }
+        else
+        {
+            $session->set('panier', $panier);
+        }
+
+        $session->set('panier_count',  $panier_count);
+
+        return $this->json([
+            'status'  => 'success',
+            'message' => 'L\'article a bien été supprimé du panier'
+        ], 200);
     }
 }
